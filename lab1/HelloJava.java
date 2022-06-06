708a06170c5682806fbf3666f2f456f934ebf3a7
jiashu2000 <jiashuchen0414@gmail.com>
2022-06-06 11:25:16 +0800

    completed first part of the lab1

---
+++ lab1/HelloJava.java
@@ -0,0 +1,19 @@
+package java001;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
+
+/* 
+1. All code in Java must be part of a class.
+
+2. We delimit the beginning and end of segments of code
+using { and }.
+
+3. All statements in Java must end in a semi-colon.
+
+4. For code to run we need pulic static void main(String)
+*/
---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,26 @@
+package java001;
+
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int sum = 0;
+		while (x < 10) {
+			sum = sum + x;
+			System.out.println(sum);
+			x = x + 1;
+		}
+		System.out.println(5 + "20");
+		System.out.println(5 + 10);
+	}
+
+}
+
+/*
+1. Before Java variables can be used, they must be declared.
+
+2. Java variables must have a specific type.
+
+3. Java variable types can never change.
+
+4. Types are verified before the code even runs!!!
+*/
